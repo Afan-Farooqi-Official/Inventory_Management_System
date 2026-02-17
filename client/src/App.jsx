b0f874d17cc225a597cbe57ac3a752707c6eda31
Afan-Farooqi-Official <affanqaisar000@gmail.com>
2026-02-17 15:39:05 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,33 @@
+import {Toaster} from 'react-hot-toast'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import Products from './pages/Products'
+import Categories from './pages/Categories'
+import Orders from './pages/Orders'
+import Suppliers from './pages/Suppliers'
+import Users from './pages/Users'
+import Profile from './pages/Profile'
+
+const App = () => {
+  return (
+    <div>
+      <Toaster />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ims' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='products' element={<Products />} />
+          <Route path='categories' element={<Categories />} />
+          <Route path='orders' element={<Orders />} />
+          <Route path='suppliers' element={<Suppliers />} />
+          <Route path='users' element={<Users />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
